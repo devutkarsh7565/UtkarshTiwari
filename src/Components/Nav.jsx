f99c29f647fc5ba3023ca70467edcff06af16f88
devutkarsh7565 <utkarshtiwari200389@gmail.com>
2022-06-27 23:27:29 +0530

    adding DarkMode color to skill,home,education component

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -1,20 +1,33 @@
-import useDarkMode from "../Hooks/useDarkMode";
+import { useDarkMode } from "../Hooks/useDarkMode";
 import { FaRegLightbulb } from "react-icons/fa";
 
-const Nav = () => {
-  const [colorTheme, setTheme] = useDarkMode();
+// import { useEffect } from "react";
+
+const Nav = (props) => {
+  const darkMode = useDarkMode();
+  const handleChange = () => {
+    darkMode.switchTheme();
+  };
+  // useEffect(() => {
+  //   darkMode.isDarkMode = false;
+  // }, []);
   return (
     <>
-      <div className="fixed top-0 py-5 w-full">
-        <span
-          onClick={() => setTheme(colorTheme)}
-          className="flex justify-start"
-        >
-          {colorTheme === "light" ? (
+      <div className="fixed top-0 py-5 w-full flex justify-between ">
+        <span onClick={handleChange} className="flex justify-start">
+          {darkMode.isDarkMode === true ? (
             <FaRegLightbulb className="bg-white rounded-full shadow-lg cursor-pointer text-indigo-500 flex justify-center items-center p-2 w-10 h-10" />
           ) : (
             <FaRegLightbulb className="bg-indigo-500 rounded-full shadow-lg cursor-pointer text-white flex justify-center items-center p-2 w-10 h-10" />
           )}
+        </span>
+        <span className="mr-14 mt-2 text-xl font-light">
+          <a
+            href="#"
+            className="px-4 py-3 rounded-xl bg-blue-600 text-white dark:bg-white dark:text-blue-600 border-3 dark:border-gray-200 "
+          >
+            Resume
+          </a>
         </span>
       </div>
     </>
@@ -22,3 +35,28 @@ const Nav = () => {
 };
 
 export default Nav;
+
+// import useDarkMode from "../Hooks/useDarkMode";
+// import { FaRegLightbulb } from "react-icons/fa";
+
+// const Nav = () => {
+//   const [colorTheme, setTheme] = useDarkMode();
+//   return (
+//     <>
+//       <div className="fixed top-0 py-5 w-full">
+//         <span
+//           onClick={() => setTheme(colorTheme)}
+//           className="flex justify-start"
+//         >
+//           {colorTheme === "light" ? (
+//             <FaRegLightbulb className="bg-white rounded-full shadow-lg cursor-pointer text-indigo-500 flex justify-center items-center p-2 w-10 h-10" />
+//           ) : (
+//             <FaRegLightbulb className="bg-indigo-500 rounded-full shadow-lg cursor-pointer text-white flex justify-center items-center p-2 w-10 h-10" />
+//           )}
+//         </span>
+//       </div>
+//     </>
+//   );
+// };
+
+// export default Nav;
--- src/Hooks/useDarkMode.js
+++ src/Hooks/useDarkMode.js
@@ -1,12 +1,26 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
+export const useDarkMode = () => {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+  const switchTheme = () => {
+    if (isDarkMode) {
+      const root = document.documentElement;
+      root.classList.remove("dark");
+      setIsDarkMode(false);
+    } else {
+      document.documentElement.classList.add("dark");
+      setIsDarkMode(true);
+    }
+  };
+  return { isDarkMode, switchTheme };
+};
 
-export default function useDarkMode() {
-  const [theme, setTheme] = useState("light");
-  const colorTheme = theme === "dark" ? "light" : "dark";
-  useEffect(() => {
-    const root = window.document.documentElement;
-    root.classList.remove(colorTheme);
-    root.classList.add(theme);
-  }, [theme, colorTheme]);
-  return [colorTheme, setTheme];
-}
+// export default function useDarkMode() {
+//   const [theme, setTheme] = useState("light");
+//   const colorTheme = theme === "dark" ? "light" : "dark";
+//   useEffect(() => {
+//     const root = window.document.documentElement;
+//     root.classList.remove(colorTheme);
+//     root.classList.add(theme);
+//   }, [theme, colorTheme]);
+//   return [colorTheme, setTheme];
+// }
